621b24c4c473f81ce23f79a4f1707919aa37f536
paul <themyth92@gmail.com>
2019-06-19 14:01:27 +0800

    Change index

--- index.d.ts
+++ index.d.ts
@@ -4,27 +4,17 @@ import Op = require('./lib/operators');
 import QueryTypes = require('./lib/query-types');
 import TableHints = require('./lib/table-hints');
 import IndexHints = require('./lib/index-hints');
-import * as Association from './lib/associations';
-import * as Error from './lib/errors';
-import * as Model from './lib/model';
-import * as Promise from './lib/promise';
-import * as QueryInterface from './lib/query-interface';
-import * as Sequelize from './lib/sequelize';
-import * as Transaction from './lib/transaction';
-import * as ValidatorExtra from './lib/utils/validator-extras';
-
 import Utils = require('./lib/utils');
-declare module 'sequelize' {
-  export * from './lib/sequelize';
-  export * from './lib/query-interface';
-  export * from './lib/data-types';
-  export * from './lib/model';
-  export * from './lib/transaction';
-  export * from './lib/associations';
-  export * from './lib/errors';
-  export { BaseError as Error } from './lib/errors';
-  export { useInflection } from './lib/utils';
-  export { Promise } from './lib/promise';
-  export { Utils, QueryTypes, Op, TableHints, IndexHints, DataTypes, Deferrable };
-  export { Validator as validator } from './lib/utils/validator-extras';
-}
+
+export * from './lib/sequelize';
+export * from './lib/query-interface';
+export * from './lib/data-types';
+export * from './lib/model';
+export * from './lib/transaction';
+export * from './lib/associations/index';
+export * from './lib/errors';
+export { BaseError as Error } from './lib/errors';
+export { useInflection } from './lib/utils';
+export { Promise } from './lib/promise';
+export { Utils, QueryTypes, Op, TableHints, IndexHints, DataTypes, Deferrable };
+export { Validator as validator } from './lib/utils/validator-extras';
